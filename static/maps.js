130c744a3964f379641b330e9e297ecd6e920c11
Zunayed Ali <zunayed@gmail.com>
2013-12-16 18:32:40 -0500

    added ability to zoom & created a legend

--- static/maps.js
+++ static/maps.js
@@ -6,6 +6,7 @@ var dataSetIndex = 0;
 var svg;
 var zips;
 
+//map type & center point 
 var projection = d3.geo.mercator()
 	.center([-73.955541, 40.795780])
     .scale(95000);
@@ -13,7 +14,16 @@ var projection = d3.geo.mercator()
 var path = d3.geo.path().projection(projection);
 
 //loading up different datasets, default color and max domain value
-var data = [[noiseData, "Blues", 180], [heatData, "Reds", 450], [graffitiData, "RdPu", 40]];
+var data = [[noiseData, "Blues", 180], [heatData, "Reds", 430], [graffitiData, "RdPu", 40]];
+var dataLegendMin;
+var dataLegendMax;
+
+var findLegendMaxMin = function (i) {
+	dataLegendMax = data[dataSetIndex][2];
+	console.log ("max", dataLegendMax)
+	dataLegendMin = 0;
+}
+findLegendMaxMin(dataSetIndex);
 
 //set up a qX-X number to associate with colorbrew.css styles
 var setColor = d3.scale.quantize()
@@ -31,6 +41,46 @@ var enableHover = function () {
 	);
 };
 
+var initializeSVG = function () {
+
+	svg = d3.select("#d3_map")
+		.append("svg")
+		.attr("class", "Blues")
+		.attr("width", w)
+		.attr("height", h);
+};
+var createLegend = function () {
+
+	var nsteps = 10;
+	var step = dataLegendMax / nsteps;
+	var legendRange = [];
+	for (k = 0 ; k < nsteps ; k++) {
+		legendRange.push (Math.floor(dataLegendMin + k * step));
+	}
+
+	var legend = svg.selectAll("g.legend")
+  .data(legendRange)
+  .enter().append("g")
+  .attr("class", "legend");
+
+  var ls_w = 20, ls_h = 20;
+
+  legend.append("rect")
+  .attr("x", 20)
+  .attr("y", function(d, i){ return h/2 - (i*ls_h) - 2*ls_h;})
+  .attr("width", ls_w)
+  .attr("height", ls_h)
+  .attr("class", function(d, i) { return setColor(d); })
+ // .attr("class", "q4-9")
+
+  legend.append("text")
+  .attr("x", 50)
+  .attr("y", function(d, i){ return h/2 - (i*ls_h) - ls_h - 4;})
+  .attr("class", "mapSubtext")
+
+  .text(function(d, i){ return d});
+}
+
 //map number of complaint to color intensity
 var zipcodeColor = function(zip, data) {
 	if(zip in data){	
@@ -41,33 +91,47 @@ var zipcodeColor = function(zip, data) {
 	}
 };
 
-var initSVG = function () {
-	svg = d3.select("#d3_map")
-			.append("svg")
-			.attr("class", "Blues")
-			.attr("width", w)
-			.attr("height", h);
+function zoom() {
+	// console.log("zooming", d3.event.scale, d3.event.translate);
+	trans = d3.event.translate;
+	// trans[0] = Math.min(trans[0], d3.event.scale * 400);
+	// trans[0] = Math.max(trans[0], d3.event.scale * -400);
+	// console.log(trans);
+	svg.select("g").attr("transform", "translate(" + trans + ")scale(" + d3.event.scale + ")");
+	d3.selectAll("svg").attr("stroke-width", ''+(1.75/d3.event.scale)+'px');
 }
-	
-var createMap = function (zipcodes) {
 
-	//svg.remove();
+var findCentre = function (d) {
+
+
+}
+var createMap = function (zipcodes) {
+	svg.remove();
+	initializeSVG();
+
+	svg.call(d3.behavior.zoom().scaleExtent([.9, 8]).on("zoom", zoom))
 
 	svg.append("g")
 		.selectAll("path")
 		.data(zipcodes.features)
 		.enter()
 		.append("path")
-		  .attr("title", function(d) { return d.id })
-		  .attr("class", function(d) { return zipcodeColor(d.id, (data[dataSetIndex])[0]); } )
-		  .attr("stroke", '#fff')
-		  .attr("stroke-width", '1.75px')
-
+	  	.attr("title", function(d) { return d.id })
+	  	.attr("class", function(d) { return zipcodeColor(d.id, (data[dataSetIndex])[0]); } )
+	  	.attr("stroke", '#fff')
+	  	.attr("bbox", function (d) { console.log(d);
+	  								var b = d3.geo.bounds(d);
+	  									console.log (b);
+	  									return b;
+	  								})
+	  //	.attr("stroke-width", '1.75px')
 		.attr("d", path);
+
+	createLegend();
 	enableHover();
 }
 
-initSVG();
+initializeSVG(); 
 
 //reading geoJSON file and assigns it to zipcode
 d3.json('static/data/zipcodes.json', function(zipcodes){
@@ -84,9 +148,12 @@ d3.select("#colorSelector").on("change", function() {
 d3.select("#dataSelector").on("change", function() {
 	dataSetIndex = parseInt(this.value);
 
+	findLegendMaxMin(dataSetIndex);
+
 	setColor.domain([0, data[dataSetIndex][2]])
 	console.log(data[dataSetIndex])
 	createMap(zips);
 
 	svg.attr("class", data[dataSetIndex][1]);
 });
+
