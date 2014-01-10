e27c977a127c8c18146afd5f6e5fe3f700b1aee3
Zunayed <zunayed@gmail.com>
2014-01-10 00:38:15 -0500

    1. rearranged function order to make a bit more sense 2. Replaced array of data sets with a object to make code more readable

--- static/maps.js
+++ static/maps.js
@@ -1,22 +1,38 @@
-//loading up different datasets, default color and max domain value
 /*global noiseData: false, heatData: false, graffitiData: false, d3: false  */
 "use strict";
 
-var data = [[noiseData, "Blues", 180], [heatData, "Reds", 450], [graffitiData, "RdPu", 40]];
+//loading up different datasets, default color and max domain value
+var dataSets = {
+	"noise": {
+		"data": noiseData,
+		"color": "Blues",
+		"maxDomain": 180
+	},
+	"heat": {
+		"data": heatData,
+		"color": "Reds",
+		"maxDomain": 375
+	},
+	"graffiti": {
+		"data": graffitiData,
+		"color": "RdPu",
+		"maxDomain": 30
+	}
+};
 
 //set up map
 var w = screen.width;
 var h = screen.height;
 
-var dataSetIndex = 0;
+var currentData = dataSets.noise;
 var svg;
 var zips;
 
 //legend settings
-var dataLegendMax = data[dataSetIndex][2];
-var ls_w = 20;
-var ls_h = 20;
-var nsteps = 9;
+var dataLegendMax = currentData.maxDomain;
+var legendWidth = 20;
+var legendHeight = 20;
+var legendSteps = 9;
 
 //map type & center point 
 var projection = d3.geo.mercator()
@@ -27,11 +43,11 @@ var path = d3.geo.path().projection(projection);
 
 //set up a qX-X number to associate with colorbrew.css styles
 var setColor = d3.scale.quantize()
-    .domain([0, data[dataSetIndex][2]])
+    .domain([0, currentData.maxDomain])
     .range(d3.range(9).map(function(i) { return "q" + (i) + "-9"; }));
 
-var initializeSVG = function () {
 
+var initializeSVG = function () {
 	svg = d3.select("#d3_map")
 		.append("svg")
 		.attr("class", "Blues")
@@ -39,12 +55,47 @@ var initializeSVG = function () {
 		.attr("height", h);
 };
 
+
+var createMap = function (zipcodes) {
+	svg.remove();
+	initializeSVG();
+
+	svg.append("g")
+		.selectAll("path")
+		.data(zipcodes.features)
+		.enter()
+		.append("path")
+		.attr("title", function(d) { return d.id; })
+		.attr("class", function(d) { return zipcodeColor(d.id, currentData.data); } )
+		.attr("stroke", "#fff")
+		.on("mouseover", mouseover)
+		.on("mouseout", mouseout)
+		.attr("d", path);
+
+	//scaleExtent is the max and min of zoom level
+	svg.call(d3.behavior.zoom().scaleExtent([1/2, 8]).on("zoom", zoom));
+
+	createLegend();
+};
+
+
+//map number of complaint to color intensity
+var zipcodeColor = function(zip, data) {
+	if(zip in data){
+		return setColor(data[zip]);
+	}else{
+		//no data
+		return "white";
+	}
+};
+
+
 var createLegend = function () {
 
-	var step = dataLegendMax / nsteps;
+	var step = dataLegendMax / legendSteps;
 	var legendRange = [];
 
-	for (var k = 0 ; k < nsteps ; k++) {
+	for (var k = 0 ; k < legendSteps ; k++) {
 		legendRange.push (Math.floor(k * step));
 	}
 
@@ -55,26 +106,25 @@ var createLegend = function () {
 
 	legend.append("rect")
 		.attr("x", 20)
-		.attr("y", function(d, i){ return h/4 - (i*ls_h) - 2*ls_h; })
-		.attr("width", ls_w)
-		.attr("height", ls_h)
+		.attr("y", function(d, i){ return h/4 - (i*legendHeight) - 2*legendHeight; })
+		.attr("width", legendWidth)
+		.attr("height", legendHeight)
 		.attr("class", function(d) { return setColor(d); });
 
 	legend.append("text")
 		.attr("x", 40 + 2)
-		.attr("y", function(d, i){ return h/4 - (i*ls_h) - 2*ls_h + 15; })
+		.attr("y", function(d, i){ return h/4 - (i*legendHeight) - 2*legendHeight + 15; })
 		.attr("class", "mapSubtext")
 		.text(function(d){ return d;});
 };
 
-//map number of complaint to color intensity
-var zipcodeColor = function(zip, data) {
-	if(zip in data){
-		return setColor(data[zip]);
-	}else{
-		//no data
-		return "white";
-	}
+
+var zoom = function() {
+	var trans = d3.event.translate;
+	svg.select("g")
+		.attr("transform", "translate(" + trans + ")scale(" + d3.event.scale + ")");
+	//scale stroke width based on zoom
+	d3.selectAll("#d3_map").attr("stroke-width", ""+ (1.75/d3.event.scale) +"px");
 };
 
 //Jquery hover way
@@ -92,47 +142,20 @@ var zipcodeColor = function(zip, data) {
 	
 // };
 
+
 //D3 hover info way
-function mouseover(){
+var mouseover = function() {
 	d3.select(this).style("stroke-width", "4px");
 	var zip = d3.select(this).attr("title");
-	var complaintCount = data[dataSetIndex][0][zip];
+	var complaintCount = currentData.data[zip];
 	d3.select("#infoBox").html("zip " + zip + " complaints " + complaintCount);
+};
 
-}
 
-function mouseout(){
+var mouseout = function() {
 	d3.select(this).style("stroke-width", "");
-}
-
-var zoom = function() {
-	var trans = d3.event.translate;
-	svg.select("g")
-		.attr("transform", "translate(" + trans + ")scale(" + d3.event.scale + ")");
-	//scale stroke width based on zoom
-	d3.selectAll("#d3_map").attr("stroke-width", ""+ (1.75/d3.event.scale) +"px");
 };
 
-var createMap = function (zipcodes) {
-	svg.remove();
-	initializeSVG();
-
-	svg.call(d3.behavior.zoom().scaleExtent([1/2, 8]).on("zoom", zoom));
-
-	svg.append("g")
-		.selectAll("path")
-		.data(zipcodes.features)
-		.enter()
-		.append("path")
-		.attr("title", function(d) { return d.id; })
-		.attr("class", function(d) { return zipcodeColor(d.id, (data[dataSetIndex])[0]); } )
-		.attr("stroke", "#fff")
-		.on("mouseover", mouseover)
-		.on("mouseout", mouseout)
-		.attr("d", path);
-
-	createLegend();
-};
 
 //create svg element
 initializeSVG();
@@ -140,21 +163,23 @@ initializeSVG();
 //reading geoJSON file and assigns it to zipcode
 d3.json("static/data/zipcodes.json", function(zipcodes){
 	zips = zipcodes;
-	return createMap(zipcodes);
+	return createMap(zips);
 });
+
 
 //monitor dropdown menu to change map colors
 d3.select("#colorSelector").on("change", function() {
   d3.selectAll("svg").attr("class", this.value);
 });
 
+
 //monitor dropdown menu to change map data
 d3.select("#dataSelector").on("change", function() {
-	dataSetIndex = parseInt(this.value);
-	dataLegendMax = data[dataSetIndex][2];
+	currentData = dataSets[this.value];
+	dataLegendMax = currentData.maxDomain;
 
-	setColor.domain([0, data[dataSetIndex][2]]);
+	setColor.domain([0, currentData.maxDomain]);
 	createMap(zips);
 
-	svg.attr("class", data[dataSetIndex][1]);
+	svg.attr("class", currentData.color);
 });
