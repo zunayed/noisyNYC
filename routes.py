7d140434c6cf6216744c05a78b710dfba94b8ba9
Zunayed Ali <zunayed@gmail.com>
2014-10-27 15:53:05 -0400

    adding time page

--- routes.py
+++ routes.py
@@ -2,10 +2,15 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/', methods=['GET', 'POST'])
 def home():
-	return render_template('home.html')
+    return render_template('home.html')
+
+
+@app.route('/time', methods=['GET', ])
+def time():
+    return render_template('time.html')
 
 if __name__ == '__main__':
     app.run()
-
